2cf7a29bcb1fd6bd762d7a93eecdf07af79064b9
Rizky Ananta <anantaristik@gmail.com>
2023-04-19 05:46:53 +0700

    Initial commit added react, vite config, tailwind css, added navbar and search bar FE

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './Components/NavBar/Navbar'
+import Search from './Components/SearchDiv/Search'
+import Jobs from './Components/JobDiv/Job'
+import Value from './Components/ValueDiv/Value'
+import Footer from './Components/FooterDiv/Footer'
+
+const App = () => {
+  return (
+    <div className='w-[85%] m-auto bg-white'>
+      <Navbar/>
+      <Search/>
+      <Jobs/>
+      <Value/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/SearchDiv/Search.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { AiOutlineSearch, AiOutlineCloseCircle } from 'react-icons/ai'
+import { BsHouseDoor } from 'react-icons/bs'
+import { CiLocationOn } from 'react-icons/ci'
+
+const Search = () => {
+  return (
+    <div className='searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[3rem]'>
+      <form action="">
+        <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px]
+          bg-white p-5 shhadow-lg shadow-greyIsh-700'>
+
+            <div className='flex gap-2 items-center'>
+              <AiOutlineSearch className='text-[25px] icon'/>
+              <input type='text' className='bg-transparent text-blue-500 focus:outline-none w-[100%]'
+               placeholder='Search for jobs here....'/> 
+                <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a5]
+                hover:text-textColor icon'/>
+            </div>
+
+            <div className='flex gap-2 items-center'>
+              <BsHouseDoor className='text-[25px] icon'/>
+              <input type='text' className='bg-transparent text-blue-500 focus:outline-none w-[100%]'
+               placeholder='Search by company....'/> 
+                <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a5]
+                hover:text-textColor icon'/>
+            </div>
+
+            <div className='flex gap-2 items-center'>
+              <CiLocationOn className='text-[25px] icon'/>
+              <input type='text' className='bg-transparent text-blue-500 focus:outline-none w-[100%]'
+               placeholder='Search by location....'/> 
+                <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a5]
+                hover:text-textColor icon'/>
+            </div>
+
+            <button className='bg-blueColor h-full p-5 px-10 rounded-[10px] text-white cursor-pointer hover:bg-blue-300'>Search</button>
+
+         </div> 
+      </form>
+
+    </div>
+  )
+}
+
+export default Search
